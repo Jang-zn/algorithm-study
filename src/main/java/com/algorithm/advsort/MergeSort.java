4cf5966f01b1603ad25d09b08800dde060a09f00
Jang <zn2309@naver.com>
2021-08-29 17:17:03 +0900

    FastCampus 11. 고급 Sort - MergeSort
    재귀호출을 이용한 정렬방식
    merge 함수에서 while문 조건 잘못걸어서 헤맸음

---
+++ src/main/java/com/algorithm/advsort/MergeSort.java
@@ -0,0 +1,44 @@
+package com.algorithm.advsort;
+
+import java.util.ArrayList;
+
+public class MergeSort {
+    int count=0;
+    static boolean check=false;
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=0;i<16;i++){
+            list.add((int)(Math.random()*1000)+1);
+        }
+        System.out.println(list);
+        System.out.println(mergeSort(list));
+
+    }
+    public static ArrayList<Integer> mergeSort(ArrayList<Integer> list){
+        if(list.size()<=1){
+            return list;
+        }else{
+             ArrayList<Integer> left = mergeSort(new ArrayList<Integer>(list.subList(0,list.size()/2)));
+             ArrayList<Integer> right = mergeSort(new ArrayList<Integer>(list.subList(list.size()/2,list.size())));
+             return merge(left,right);
+        }
+    }
+
+    public static ArrayList<Integer> merge(ArrayList<Integer> left,ArrayList<Integer> right){
+        ArrayList<Integer> mergeList = new ArrayList<Integer>();
+        int lidx = 0;
+        int ridx = 0;
+        while(lidx<left.size()||ridx<right.size()){
+            if(lidx>=left.size()){
+                mergeList.add(right.get(ridx++));
+            }else if(ridx>=right.size()){
+                mergeList.add(left.get(lidx++));
+            }else if(left.get(lidx)<right.get(ridx)){
+                mergeList.add(left.get(lidx++));
+            }else{
+                mergeList.add(right.get(ridx++));
+            }
+        }
+        return mergeList;
+    }
+}
--- src/main/java/com/algorithm/sort/BubbleSort.java
+++ src/main/java/com/algorithm/sort/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList();
-        for(int i=0;i<20;i++){
+        for(int i=0;i<3;i++){
             list.add((int)(Math.random()*100)+1);
         }
         list = sort(list);
@@ -19,7 +19,7 @@ public class BubbleSort {
     public static ArrayList<Integer> sort(ArrayList<Integer> list){
         for(int i =0;i<list.size()-1;i++){
             boolean check = false;
-            for(int j = 0; j<list.size()-i;j++){
+            for(int j = 0; j<list.size()-1;j++){
                 if(list.get(j)>list.get(j+1)){
                     Collections.swap(list,j,j+1);
                     check=true;
