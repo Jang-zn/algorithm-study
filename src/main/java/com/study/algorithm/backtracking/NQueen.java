b0146b76c71b2ad257f68183a8d1f5c3ab9d187c
Jang <zn2309@naver.com>
2021-09-02 17:11:18 +0900

    FastCampus 17.BackTracking - 백 트래킹 / 퇴각검색 : N Queen 문제
    - 경우의 수를 Tree로 구현(State space tree)하고 DFS 탐색.
      제약조건에 걸리면 해당 루트는 무시하고 다음 루트 검색하는 방식

---
+++ src/main/java/com/study/algorithm/backtracking/NQueen.java
@@ -0,0 +1,38 @@
+package com.study.algorithm.backtracking;
+
+import java.util.ArrayList;
+
+public class NQueen {
+
+    public static void main(String[] args) {
+        nqueen(4, 0, new ArrayList<Integer>());
+    }
+
+    public static void nqueen(int N, int currRow, ArrayList<Integer> queenList){
+        if(currRow==N){
+            System.out.println(queenList);
+            return;
+        }
+        for(int i=0; i<N;i++){
+            if(isOkay(queenList, i)){
+                queenList.add(i);
+                nqueen(N,currRow+1, queenList);
+                //Pruning
+                queenList.remove(queenList.size()-1);
+            }
+        }
+
+    }
+
+    //Promising
+    public static boolean isOkay(ArrayList<Integer> queenList, int currCol){
+        int currRow = queenList.size();
+        for(int i=0;i<currRow;i++){
+            //수직 or 대각선일때
+            if(queenList.get(i)==currCol||Math.abs(queenList.get(i)-currCol)==currRow-i){
+                return false;
+            }
+        }
+        return true;
+    }
+}
