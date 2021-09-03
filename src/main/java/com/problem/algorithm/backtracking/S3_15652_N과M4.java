5b825160755d748b3201592f291d8bce684a823c
Jang <zn2309@naver.com>
2021-09-03 22:28:19 +0900

    BOJ S3-N과M 4 1~N까지 M개를 중복 포함해서 고르는 방법
    N과 M1~4는 비슷한 유형에 조건만 변화된 것이므로 구현하는 방법 확실히 짚고 넘어갈 것

--- src/main/java/com/problem/algorithm/backtracking/S3_15652_N과M4.java
+++ src/main/java/com/problem/algorithm/backtracking/S3_15652_N과M4.java
@@ -3,9 +3,36 @@ package com.problem.algorithm.backtracking;
 import java.io.*;
 
 public class S3_15652_N과M4 {
+    static int N;
+    static int M;
+    static int[] list;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+        list = new int[M+1];
+        solve(1, bw);
+        bw.flush();
+    }
+
+    public static void solve(int k, BufferedWriter bw) throws IOException{
+        if(k==M+1){
+            for(int i=1;i<list.length;i++){
+                bw.write(String.valueOf(list[i]));
+                bw.write(" ");
+            }
+            bw.write("\n");
+            return;
+        }else{
+            for(int i=1;i<=N;i++){
+                if(list[k-1]<=i) {
+                    list[k] = i;
+                    solve(k + 1, bw);
+                }
+            }
+        }
     }
 }
