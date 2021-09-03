cc3d18e2bd78227cd1a406e634d0cd393d12051f
Jang <zn2309@naver.com>
2021-09-03 22:24:13 +0900

    BOJ S3-N과M 2 1~N까지 M개를 중복 없이 고르는 방법(순서 달라도 같은거 고르면 같은취급) -> '순서 다를때' 조건 빼먹었었음  (앞에꺼보다 큰 값만 들어가도록 수정)

--- src/main/java/com/problem/algorithm/backtracking/S3_15650_N과M2.java
+++ src/main/java/com/problem/algorithm/backtracking/S3_15650_N과M2.java
@@ -3,9 +3,46 @@ package com.problem.algorithm.backtracking;
 import java.io.*;
 
 public class S3_15650_N과M2 {
+    static int N;
+    static int M;
+    static int[] list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
+        String[] p = br.readLine().split(" ");
+        N = Integer.parseInt(p[0]);
+        M = Integer.parseInt(p[1]);
+        list = new int[M+1];
+        //1~N까지 M칸에 출력
+        nm(1, bw);
+        bw.flush();
+    }
+
+    public static void nm(int k, BufferedWriter bw) throws IOException{
+        if(k==M+1){
+            for(int i=1;i<list.length;i++){
+                bw.write(list[i]+" ");
+            }
+            bw.write("\n");
+            return;
+        }else{
+            for(int i=k;i<=N;i++){
+                boolean check = false;
+                for(int j=1;j<k;j++){
+                    if(list[j]==i){
+                        check=true;
+                        break;
+                    }else{
+                        check=false;
+                    }
+                }
+                if(!check) {
+                    if(list[k-1]<i) {
+                        list[k] = i;
+                        nm(k + 1, bw);
+                    }
+                }
+            }
+        }
     }
 }
