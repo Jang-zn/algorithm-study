98458ab1fbce9b4d9aa8ae3b059364017da64972
Jang <zn2309@naver.com>
2021-09-03 21:38:24 +0900

    BOJ S2 - 좌표압축
     - 자꾸 시간초과남
     -> 풀이 참고해서 해결
        1. ArrayList 받고 하나 복사해서 정렬 (여기까진 같음)
        2. 'HashMap' 사용해서 정렬된거 반복문으로 탐색, Map에 키 있는지 확인하고 없으면 count++ 해주며 삽입
           - ex) 1 3 5 7 9 9 인 경우 1=0, 3=1, 5=2, 7=3, 9=4 가 저장됨.
        3. 출력

--- src/main/java/com/problem/algorithm/sort/S2_18870_좌표압축.java
+++ src/main/java/com/problem/algorithm/sort/S2_18870_좌표압축.java
@@ -1,5 +1,33 @@
 package com.problem.algorithm.sort;
 
-public class S2_18870_좌표압축 {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
+public class S2_18870_좌표압축 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        String[] temp = br.readLine().split(" ");
+        ArrayList<Integer> p = new ArrayList<Integer>();
+        HashMap<Integer, Integer> sortP = new HashMap<Integer, Integer>();
+        for(String s : temp){
+            int n = Integer.parseInt(s);
+            p.add(n);
+        }
+        ArrayList<Integer> tempP = (ArrayList<Integer>) p.clone();
+        Collections.sort(tempP);
+        int count = 0;
+        for(int i = 0 ; i < tempP.size() ; i++) {
+            if(!sortP.containsKey(tempP.get(i))) {
+                sortP.put(tempP.get(i), count++);
+            }
+        }
+        for(int i : p){
+            bw.write(sortP.get(i)+" ");
+        }
+        bw.flush();
+    }
 }
