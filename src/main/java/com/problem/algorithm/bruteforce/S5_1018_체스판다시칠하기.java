7f59081729b4b2556cdf86efd3203f7eb3cd74f6
Jang <zn2309@naver.com>
2021-09-07 00:03:06 +0900

    BOJ S5-체스판 다시 칠하기   - 배열이랑 이중포문써서 그냥 막 만들었는데 out of index 발생(예제는 잘돌아가는데 제출하면 이럼)

--- src/main/java/com/problem/algorithm/bruteforce/S5_1018_체스판다시칠하기.java
+++ src/main/java/com/problem/algorithm/bruteforce/S5_1018_체스판다시칠하기.java
@@ -1,12 +1,15 @@
 package com.problem.algorithm.bruteforce;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class S5_1018_체스판다시칠하기 {
     static int N;
     static int M;
     static String[][] chess = new String[8][8];
     static String[][] board;
+    static ArrayList<Integer> color=new ArrayList<Integer>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,6 +20,33 @@ public class S5_1018_체스판다시칠하기 {
         board = new String[N][M];
         for(int i=0;i<N;i++){
             board[i] = br.readLine().split("");
+        }
+        count(0,0,board);
+    }
+
+    public static void count(int N, int M, String[][] board){
+        if(N+8>=board.length||M+8>=board[0].length){
+            return;
+        }else {
+            for (int i = N; i < board.length - 8; i++) {
+                for (int j = M; j < board[0].length - 8; j++) {
+                    chess[i][j] = board[i][j];
+                }
+            }
+            int check = 0;
+            for (int i = 0; i < N - 1; i++) {
+                for (int j = 0; j < M - 1; j++) {
+                    if (chess[i][j].equals(chess[i + 1][j]) || chess[i][j].equals(chess[i][j + 1])) {
+                        check++;
+                    }
+                }
+            }
+            color.add(check);
+            count(N+1,M,board);
+            count(N,M+1,board);
+            count(N+1,M+1,board);
+            Collections.sort(color);
+            System.out.println(color.get(0));
         }
     }
 }
