baec623ab708980403b1058c7290b0100a484f4b
Jang <zn2309@naver.com>
2021-09-07 22:58:19 +0900

    BOJ G5 NQueen   - 반복문(boolean)+재귀 형태로 로직 작업중

--- src/main/java/com/problem/algorithm/backtracking/G5_9663_NQueens.java
+++ src/main/java/com/problem/algorithm/backtracking/G5_9663_NQueens.java
@@ -1,11 +1,27 @@
 package com.problem.algorithm.backtracking;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class G5_9663_NQueens {
+    static ArrayList board = new ArrayList();
+    static int N;
+    int count=0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+    }
+
+    public void queen(int col){
+        if(col>=N){
+            return;
+        }else{
+            boolean check = true;
+            while(check){
+
+            }
+            queen(col+1);
+        }
     }
 }
