98597201621cd26133d000f6260c032a7aeae171
Jang <zn2309@naver.com>
2021-09-08 17:24:37 +0900

    BOJ G5 NQueen   - 반복문(boolean)+재귀 형태로 로직 수립   - 이론 공부할때 봤던 Proving  - 조건에 맞는지 탐색 / Pruning - 안맞으면 루트 변경  개념 잊지 말것.   - 다 짜고 board remove 안해줘서 헤멤(pruning)

--- src/main/java/com/problem/algorithm/backtracking/G5_9663_NQueens.java
+++ src/main/java/com/problem/algorithm/backtracking/G5_9663_NQueens.java
@@ -4,24 +4,41 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class G5_9663_NQueens {
-    static ArrayList board = new ArrayList();
+    static ArrayList<Integer> board = new ArrayList<Integer>();
     static int N;
-    int count=0;
+    static int count=0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
+        queen(0);
+        System.out.println(count);
     }
 
-    public void queen(int col){
-        if(col>=N){
+    public static void queen(int currRow){
+        if(currRow>=N){
+            count++;
             return;
         }else{
-            boolean check = true;
-            while(check){
-
+            for(int i=0;i<N;i++) {
+                if (isOkay(i)) {
+                    board.add(i);
+                    queen(currRow + 1);
+                    //Pruning... 조건 안맞는 루트 기록 삭제용
+                    board.remove(board.size()-1);
+                }
             }
-            queen(col+1);
         }
+    }
+
+    public static boolean isOkay(int col){
+        int currRow = board.size();
+        for(int i=0;i<currRow;i++){
+            //수직 확인 + 대각선확인 (대각선 : 가로차이==세로차이 이면 대각선)
+            if(board.get(i)==col||Math.abs(board.get(i)-col)==currRow-i){
+                return false;
+            }
+        }
+        return true;
     }
 }
