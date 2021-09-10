4c59a607232385e6f7ba0a530b17a048e0b0eb26
Jang <zn2309@naver.com>
2021-09-11 01:57:42 +0900

    G5- 두 용액 (two pointer 이용)   - two pointer로 풀경우 O(NlogN)+O(N) = O(NlogN)     * 단 정렬 안할경우 탐색 N회 필요해서 O(N^2)임 정렬 필수     * L-> R<- 방향이므로 list.size()까지 돌리면 안됨.   - 기존방식은 O(NlogN)+O(NlogN)

---
+++ src/main/java/com/problem/algorithm/twopointer/G5_2470_두용액3.java
@@ -0,0 +1,53 @@
+package com.problem.algorithm.twopointer;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class G5_2470_두용액3 {
+    static ArrayList<Integer> list = new ArrayList<Integer>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        String[] temp = br.readLine().split(" ");
+        for(int i=0;i<N;i++){
+           int solution = Integer.parseInt(temp[i]);
+           list.add(solution);
+        }
+        Collections.sort(list);
+        int[]sol = calc();
+        System.out.println(sol[0]+" "+sol[1]);
+
+    }
+
+    public static int[] calc(){
+        int[] pair = new int[2];
+        int min = Integer.MAX_VALUE;
+        int R=list.size()-1;
+        int L=0;
+        while(L!=R){
+            int sum = list.get(L)+list.get(R);
+            if(sum>0){
+                if(sum<min){
+                    min=sum;
+                    pair[0]=list.get(L);
+                    pair[1]=list.get(R);
+                }
+                R--;
+            }else if(sum<0){
+                if(Math.abs(sum)<min){
+                    min=Math.abs(sum);
+                    pair[0]=list.get(L);
+                    pair[1]=list.get(R);
+                }
+                L++;
+            }else{
+                pair[0]=list.get(L);
+                pair[1]=list.get(R);
+                return pair;
+            }
+        }
+        return pair;
+    }
+}
