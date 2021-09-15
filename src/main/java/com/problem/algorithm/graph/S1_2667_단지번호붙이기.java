3cf62df1a146a9d6429536ac425b57f67d65e264
Jang <zn2309@naver.com>
2021-09-15 23:34:26 +0900

    S1 - 단지번호붙이기    - 격자형 그래프 이용하여 풀어낸다.

---
+++ src/main/java/com/problem/algorithm/graph/S1_2667_단지번호붙이기.java
@@ -0,0 +1,11 @@
+package com.problem.algorithm.graph;
+
+import java.io.*;
+
+public class S1_2667_단지번호붙이기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+    }
+}
