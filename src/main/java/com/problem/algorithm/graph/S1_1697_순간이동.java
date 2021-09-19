e9b4ea7ebb9847f74c9dfe42f2334b6d39f3473f
Jang <zn2309@naver.com>
2021-09-19 14:15:34 +0900

    S1 - 순간이동   - 그래프 문제인데 그래프를 안줌 -> 구현해야됨   + bfs 구현은 했는데 입력값(두명 위치)이 작은거 -> 큰거 일때만 성립     큰거->작은거일땐??

---
+++ src/main/java/com/problem/algorithm/graph/S1_1697_순간이동.java
@@ -0,0 +1,51 @@
+package com.problem.algorithm.graph;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class S1_1697_순간이동 {
+
+    static int N;
+    static int M;
+    static int minT=Integer.MAX_VALUE;
+    static int[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] temp = br.readLine().split(" ");
+        N = Integer.parseInt(temp[0]);
+        M = Integer.parseInt(temp[1]);
+        visit = new int[M+1];
+        for(int i=0;i<M+1;i++){
+            visit[i]=-1;
+        }
+        solve();
+        System.out.println(visit[M]);
+    }
+
+    public static void solve(){
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(N);
+        visit[N] = 0;
+        while(!q.isEmpty()){
+            int x = q.poll();
+            int r = x+1;
+            int l = x-1;
+            int teleport = 2*x;
+            if(0<=r&&r<M+1&&visit[r]==-1){
+                visit[r]=visit[x]+1;
+                q.add(r);
+            }
+            if(0<=l&&l<M+1&&visit[l]==-1){
+                visit[l]=visit[x]+1;
+                q.add(l);
+            }
+            if(0<=teleport&&teleport<M+1&&visit[teleport]==-1){
+                visit[teleport]=visit[x]+1;
+                q.add(teleport);
+            }
+        }
+    }
+}
