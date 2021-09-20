43f3967492b9018d47534bb17fdd6e23e72a918f
Jang <zn2309@naver.com>
2021-09-20 13:28:06 +0900

    S1 - 순간이동   - 배열 크기에 제한두면 안됐음(최대치 100001로 잡아야 함)     -> 넘어갔다가 돌아오는게 더 빠른 경우도 있다.

--- src/main/java/com/problem/algorithm/graph/S1_1697_순간이동.java
+++ src/main/java/com/problem/algorithm/graph/S1_1697_순간이동.java
@@ -8,7 +8,6 @@ public class S1_1697_순간이동 {
 
     static int N;
     static int M;
-    static int minT=Integer.MAX_VALUE;
     static int[] visit;
 
     public static void main(String[] args) throws IOException {
@@ -17,8 +16,8 @@ public class S1_1697_순간이동 {
         String[] temp = br.readLine().split(" ");
         N = Integer.parseInt(temp[0]);
         M = Integer.parseInt(temp[1]);
-        visit = new int[M+1];
-        for(int i=0;i<M+1;i++){
+        visit = new int[100001];
+        for(int i=0;i<100001;i++){
             visit[i]=-1;
         }
         solve();
@@ -34,15 +33,15 @@ public class S1_1697_순간이동 {
             int r = x+1;
             int l = x-1;
             int teleport = 2*x;
-            if(0<=r&&r<M+1&&visit[r]==-1){
+            if(0<=r&&r<100001&&visit[r]==-1){
                 visit[r]=visit[x]+1;
                 q.add(r);
             }
-            if(0<=l&&l<M+1&&visit[l]==-1){
+            if(0<=l&&l<100001&&visit[l]==-1){
                 visit[l]=visit[x]+1;
                 q.add(l);
             }
-            if(0<=teleport&&teleport<M+1&&visit[teleport]==-1){
+            if(0<=teleport&&teleport<100001&&visit[teleport]==-1){
                 visit[teleport]=visit[x]+1;
                 q.add(teleport);
             }
