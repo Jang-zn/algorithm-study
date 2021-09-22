41da5471f9f31d2e9d2b7e5c126fe5a641e45c61
Jang <zn2309@naver.com>
2021-09-22 23:20:52 +0900

    S1 경로찾기   - bfs보다 dfs가 더 직관적인거같음

--- src/main/java/com/problem/algorithm/graph/S1_11403_경로찾기.java
+++ src/main/java/com/problem/algorithm/graph/S1_11403_경로찾기.java
@@ -6,17 +6,31 @@ public class S1_11403_경로찾기 {
 
     static int N;
     static int[][] problem;
+    static int[][] sol;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
         problem = new int[N][N];
+        sol = new int[N][N];
         for (int i = 0; i < N; i++) {
+            String[] temp =br.readLine().split(" ");
             for (int j = 0; j < N; j++) {
-                String[] temp =br.readLine().split(" ");
                 problem[i][j] = Integer.parseInt(temp[j]);
+                sol[i][j] = 0;
             }
         }
+        solve(0);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(sol[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void solve(int ii){
+
     }
 }
