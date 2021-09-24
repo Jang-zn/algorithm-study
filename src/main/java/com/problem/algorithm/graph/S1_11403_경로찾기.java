ad08836616e7d129bb9d8b342cbdc1dc24075807
Jang <zn2309@naver.com>
2021-09-24 14:14:03 +0900

    S1 경로찾기   - 플로이드-와샬 알고리즘으로 푸는 문제였다고 함   - bfs / dfs로 뭔가 찝찝하던 부분이 i->j로 갈수 있는가? 를 i->k->j가 되는가? 를 판단하는 부분이었는데     이걸 어떻게 구현하나 모르겠어서 찾아보니 걍 3중for문으로 돌려버리는게 플로이드-와샬 알고리즘이라 한다.

--- src/main/java/com/problem/algorithm/graph/S1_11403_경로찾기.java
+++ src/main/java/com/problem/algorithm/graph/S1_11403_경로찾기.java
@@ -12,16 +12,16 @@ public class S1_11403_경로찾기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-        problem = new int[N][N];
-        sol = new int[N][N];
+        problem = new int[100][100];
+        sol = new int[100][100];
         for (int i = 0; i < N; i++) {
             String[] temp =br.readLine().split(" ");
             for (int j = 0; j < N; j++) {
                 problem[i][j] = Integer.parseInt(temp[j]);
-                sol[i][j] = 0;
+                sol[i][j] = Integer.parseInt(temp[j]);
             }
         }
-        solve(0);
+        solve();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 System.out.print(sol[i][j]+" ");
@@ -30,7 +30,25 @@ public class S1_11403_경로찾기 {
         }
     }
 
-    public static void solve(int ii){
-
+    public static void solve(){
+        //플로이드-와샬??
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (sol[i][k] == 1 && sol[k][j] == 1) {
+                        sol[i][j] = 1;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (sol[i][k] == 1 && sol[k][j] == 1) {
+                        sol[i][j] = 1;
+                    }
+                }
+            }
+        }
     }
 }
