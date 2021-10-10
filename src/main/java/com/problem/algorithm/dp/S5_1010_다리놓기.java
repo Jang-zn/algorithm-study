8d17394fab378c4e7d8641c3f527e8a4567f2159
Jang <zn2309@naver.com>
2021-10-10 23:57:10 +0900

    메모이제이션 해도 시간초과뜸..

--- src/main/java/com/problem/algorithm/dp/S5_1010_다리놓기.java
+++ src/main/java/com/problem/algorithm/dp/S5_1010_다리놓기.java
@@ -8,7 +8,7 @@ public class S5_1010_다리놓기 {
     static int T;
     static int N;
     static int M;
-
+    static int[][] dp = new int[30][30];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,19 +18,21 @@ public class S5_1010_다리놓기 {
             String[] temp = br.readLine().split(" ");
             N = Integer.parseInt(temp[0]);
             M = Integer.parseInt(temp[1]);
-            int sol = solve();
-            System.out.println(sol);
+            bw.write(combi(M,N)+"\n");
         }
+        bw.flush();
+    }
 
-    }
-    public static int solve(){
-        return fact(M)/(fact(N)*fact(M-N));
-    }
-    public static int fact(int n){
-        if(n==1){
-            return 1;
-        }else{
-            return n*fact(n-1);
+    static int combi(int n, int r) {
+        // 이미 탐색했던 경우 바로 반환
+        if(dp[n][r] > 0) {
+            return dp[n][r];
         }
+        // 2번 성질
+        if(n == r || r == 0) {
+            return dp[n][r] = 1;
+        }
+        // 1번 성질
+        return combi(n - 1, r - 1) + combi(n - 1, r);
     }
 }
