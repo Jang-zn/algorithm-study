a57c1c6f6dd26b421dc264f01e7a39575e9a505e
Jang <zn2309@naver.com>
2021-10-17 23:43:16 +0900

    숫자 커지는거 처리

---
+++ src/main/java/com/problem/algorithm/divideconquer/S1_1629_곱셈.java
@@ -0,0 +1,16 @@
+package com.problem.algorithm.divideconquer;
+
+import java.io.*;
+
+public class S1_1629_곱셈 {
+
+    static int[] N;
+    static int M;
+    static int[] problem;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+    }
+}
