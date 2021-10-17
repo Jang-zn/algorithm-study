f450183b087ec5e11297986e391a6a93f1af3fa0
Jang <zn2309@naver.com>
2021-10-18 00:11:18 +0900

    정수론..? A*B%C = ((A%C)*(B%C))%C 이용해서 재귀함수 구현해준다.

--- src/main/java/com/problem/algorithm/divideconquer/S1_1629_곱셈.java
+++ src/main/java/com/problem/algorithm/divideconquer/S1_1629_곱셈.java
@@ -4,13 +4,34 @@ import java.io.*;
 
 public class S1_1629_곱셈 {
 
-    static int[] N;
+    static int[] N = new int[3];
     static int M;
     static int[] problem;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
+        String[] temp = br.readLine().split(" ");
+        for (int i = 0; i < 3; i++) {
+            N[i] = Integer.parseInt(temp[i]);
+        }
+        int a = N[0];
+        int b = N[1];
+        int c = N[2];
+        long sol = solve(a,b,c);
+        System.out.println(sol);
+    }
+
+    public static long solve(int a, int b, int c){
+        if(b==0){
+            return 1;
+        }else{
+            long n = solve(a,b/2,c);
+            if(b%2==0){
+                return n*n%c;
+            }else{
+                return (n*n%c)*a%c;
+            }
+        }
     }
 }
